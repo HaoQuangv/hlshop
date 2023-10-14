7c7a522c322d92e318d8d8fad663b2df3b81597e
Bui Hao Quang <bquang280@gmail.com>
2023-10-14 16:32:25 +0700

    Fix lỗi api create product

--- api/routes/product.js
+++ api/routes/product.js
@@ -223,7 +223,7 @@ router.post('/create-product', checkAuth, checkRole, async (request, response) =
                                         .input('idAccount', request.userData.uuid)
                                         .query(queryUser);
 
-        const queryProduct = 'INSERT INTO Product(name, slogan, decription, notes, madeIn, uses, priceDisplay, id_Category, id_User) OUTPUT inserted.id  VALUES (@name, @slogan, @decription, @notes, @madeIn, @uses, @priceDisplay, @idCategory, @idUser)';
+        const queryProduct = 'INSERT INTO Product(name, slogan, decription, notes, madeIn, uses, priceDisplay, sellQuantity, id_Category, id_User) OUTPUT inserted.id  VALUES (@name, @slogan, @decription, @notes, @madeIn, @uses, @priceDisplay, @sellQuantity, @idCategory, @idUser)';
         const productResult = await database.request()
                                             .input('name', name)
                                             .input('slogan', slogan)
@@ -232,6 +232,7 @@ router.post('/create-product', checkAuth, checkRole, async (request, response) =
                                             .input('madeIn', madeIn)
                                             .input('uses', uses)
                                             .input('priceDisplay', '')
+                                            .input('sellQuantity', 0)
                                             .input('idCategory', idCategory)
                                             .input('idUser', userResult.recordset[0].id)
                                             .query(queryProduct);
@@ -243,18 +244,16 @@ router.post('/create-product', checkAuth, checkRole, async (request, response) =
                                                 .input('idProduct', productResult.recordset[0].id)
                                                 .query(updatePriceDisplay)
 
-            const insertProductSku = "INSERT INTO Product_sku(quantity, price, idAttribute1, idAttribute2, idProduct) VALUES (@quantity, @price, @idAttribute1, @idAttribute2, @idProduct)";
+            const insertProductSku = "INSERT INTO Product_sku(quantity, price, idProduct) VALUES (@quantity, @price, @idProduct)";
             const resultProductSku = await database.request()
                                                 .input('quantity', quantity[0])
                                                 .input('price', price[0])
-                                                .input('idAttribute1', "")
-                                                .input('idAttribute2', "")
                                                 .input('idProduct', productResult.recordset[0].id)
                                                 .query(insertProductSku);
         }
         else if(attribute2 === ""){
             const insertProductAttribute1 = "INSERT INTO Product_attribute1 (name, description, image, id_product) OUTPUT inserted.id VALUES (@name, @description, @image, @idProduct)";
-            const insertProductSku = "INSERT INTO Product_sku(quantity, price, idAttribute1, idAttribute2, idProduct) VALUES (@quantity, @price, @idAttribute1, @idAttribute2, @idProduct)";
+            const insertProductSku = "INSERT INTO Product_sku(quantity, price, idAttribute1, idProduct) VALUES (@quantity, @price, @idAttribute1, @idProduct)";
             for(var x = 0; x < atttributeValue1.length; x++){
                 const resultProductAttribute1 = await database.request()
                                                             .input('name', attribute1)
@@ -267,9 +266,10 @@ router.post('/create-product', checkAuth, checkRole, async (request, response) =
                                                         .input('quantity', quantity[x])
                                                         .input('price', price[x])
                                                         .input('idAttribute1', resultProductAttribute1.recordset[0].id)
-                                                        .input('idAttribute2', "")
                                                         .input('idProduct', productResult.recordset[0].id)
                                                         .query(insertProductSku);
+
+                arrayIdAttribureValue1.push(resultProductAttribute1.recordset[0].id);
             }
 
             const updatePriceDisplay = "UPDATE Product SET priceDisplay = @price WHERE id = @idProduct";
@@ -325,13 +325,14 @@ router.post('/create-product', checkAuth, checkRole, async (request, response) =
                                                 .input('price', priceDisplay)
                                                 .input('idProduct', productResult.recordset[0].id)
                                                 .query(updatePriceDisplay)
-            
-            response.status(200).json({
-                "idProduct": productResult.recordset[0].id,
-                "arrayAttributeValue1": arrayIdAttribureValue1,
-                "arrayAttributeValue1": arrayIdAttribureValue2
-            })
         }
+
+        response.status(200).json({
+            "idProduct": productResult.recordset[0].id,
+            "arrayAttributeValue1": arrayIdAttribureValue1,
+            "arrayAttributeValue2": arrayIdAttribureValue2
+        })
+
     }catch(error){
         console.log(error);
         response.status(500).json({
@@ -457,7 +458,174 @@ router.get('/get-product-sku-detail', async (request, response) => {
     }
 })
 
+router.get('/get-list-best-seller', async (request, response) => {
+    try{
+        var offset= request.query.offset;
+        var limit = request.query.limit;
 
+
+        if (offset == null || offset < 1) {
+            offset = 1;
+        }
+    
+        if (limit == null) {
+            limit = 10;
+        }
+
+        offset = (offset - 1) * limit;
+        const queryProduct = "SELECT * FROM (SELECT TOP 10 * FROM Product ORDER BY sellQuantity DESC) AS subproduct ORDER BY name OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY";
+        const resultProduct = await database.request()
+                                            .input('offset', parseInt(offset))
+                                            .input('limit', parseInt(limit))
+                                            .query(queryProduct)
+ 
+        var products = []
+
+        for (var i = 0; i < resultProduct.recordset.length; i++){
+            var medias = [];
+            var skus = [];
+
+            var queryMedia = "SELECT * FROM Media WHERE id_product = @idProduct";
+            var resultMedia = await database.request()
+                                            .input('idProduct', resultProduct.recordset[0].id)
+                                            .query(queryMedia);
+            
+            
+            var queryProductSku = "SELECT* from Product_sku WHERE idProduct =  @idProduct"
+            var resultProductSku = await database.request()
+                                                    .input('idProduct', resultProduct.recordset[0].id)
+                                                    .query(queryProductSku)
+
+            
+            for (var x = 0; x < resultMedia.recordset.length; x++){
+                var media = {};
+                media['mediaID'] = resultMedia.recordset[x].id;
+                media['linkString'] = resultMedia.recordset[x].linkString;
+                media['title'] = resultMedia.recordset[x].title;
+                media['description'] = resultMedia.recordset[x].description;
+                                    
+                medias.push(media);
+            }
+
+            for (var y = 0; y < resultProductSku.recordset.length; y++){
+                var sku = {};
+                sku['skuID'] = resultProductSku.recordset[y].id;
+                sku['quantity'] = resultProductSku.recordset[y].quantity;
+                sku['price'] = resultProductSku.recordset[y].price;
+                sku['idAttribute1'] = resultProductSku.recordset[y].idAttribute1;
+                sku['idAttribute2'] = resultProductSku.recordset[y].idAttribute2;
+    
+                skus.push(sku);
+            }
+
+            var product = {
+                "productID": resultProduct.recordset[i].id,
+                "productName": resultProduct.recordset[i].name,
+                "productDescription": resultProduct.recordset[i].decription,
+                "medias": medias,
+                "productSKU": skus
+            }
+
+            products.push(product);
+        }
+
+
+        response.status(200).json({
+             "result": products
+        })                                    
+    }catch(error){
+        console.log(error);
+        response.status(500).json({
+            "error": 'Internal Server Error'
+        })
+    }
+})
+
+router.get("/get-list-by-category", async (request, response) => {
+    try{
+        var offset= request.query.offset;
+        var limit = request.query.limit;
+
+
+        if (offset == null || offset < 1) {
+            offset = 1;
+        }
+    
+        if (limit == null) {
+            limit = 10;
+        }
+
+        offset = (offset - 1) * limit;
+        var idCategory = request.query.idCategory;
+
+        const queryProduct = "SELECT * FROM Product WHERE id_Category = @idCategory ORDER BY name OFFSET @offset ROWS FETCH NEXT @limit ROWS ONLY"
+        const resultProduct = await database.request()
+                                            .input('offset', parseInt(offset))
+                                            .input('limit', parseInt(limit))
+                                            .input('idCategory', idCategory)
+                                            .query(queryProduct)
+
+        var products = []
+
+        for (var i = 0; i < resultProduct.recordset.length; i++){
+            var medias = [];
+            var skus = [];
+                                    
+            var queryMedia = "SELECT * FROM Media WHERE id_product = @idProduct";
+            var resultMedia = await database.request()
+                                            .input('idProduct', resultProduct.recordset[0].id)
+                                            .query(queryMedia);
+                                                
+                                                
+            var queryProductSku = "SELECT* from Product_sku WHERE idProduct =  @idProduct"
+            var resultProductSku = await database.request()
+                                                .input('idProduct', resultProduct.recordset[0].id)
+                                                .query(queryProductSku)
+                                    
+                                                
+            for (var x = 0; x < resultMedia.recordset.length; x++){
+                var media = {};
+                media['mediaID'] = resultMedia.recordset[x].id;
+                media['linkString'] = resultMedia.recordset[x].linkString;
+                media['title'] = resultMedia.recordset[x].title;
+                media['description'] = resultMedia.recordset[x].description;
+                                                                        
+                medias.push(media);
+            }
+                                    
+            for (var y = 0; y < resultProductSku.recordset.length; y++){
+                var sku = {};
+                sku['skuID'] = resultProductSku.recordset[y].id;
+                sku['quantity'] = resultProductSku.recordset[y].quantity;
+                sku['price'] = resultProductSku.recordset[y].price;
+                sku['idAttribute1'] = resultProductSku.recordset[y].idAttribute1;
+                sku['idAttribute2'] = resultProductSku.recordset[y].idAttribute2;
+                                        
+                skus.push(sku);
+            }
+                                    
+            var product = {
+                "productID": resultProduct.recordset[i].id,
+                "productName": resultProduct.recordset[i].name,
+                "productDescription": resultProduct.recordset[i].decription,
+                "medias": medias,
+                "productSKU": skus
+            }
+                                    
+            products.push(product);
+        }
+                                    
+                                    
+        response.status(200).json({
+            "result": products
+        })                                        
+    }catch(error){
+        console.log(error);
+        response.status(500).json({
+            "error": 'Internal Server Error'
+        })
+    }
+})
 
 
 
