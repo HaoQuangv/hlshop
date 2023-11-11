4742ba82b96356b17e42927eda7ed31670fee39e
Thiennn262626 <ndt10a5@gmail.com>
2023-11-11 23:28:32 +0700

    fix cart

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -16,33 +16,38 @@ router.post("/add-cart", checkAuth, checkRole, async (request, response) => {
         error: "Invalid input data",
       });
     }
-
     const queryUser = "SELECT id FROM [User] WHERE id_account = @idAccount";
     const userResult = await database
       .request()
       .input("idAccount", request.userData.uuid)
       .query(queryUser);
-    const queryExitCart =
-      "SELECT id, quantity FROM Cart WHERE idProductSku = @idProductSku";
-    const exitCartResult = await database
-      .request()
-      .input("idProductSku", idProductSku)
-      .query(queryExitCart);
 
-    if (exitCartResult.recordset.length !== 0) {
-      // Nếu sản phẩm đã có trong giỏ hàng, cập nhật số lượng
-      const updatedQuantity = exitCartResult.recordset[0].quantity + quantity;
+    // Kiểm tra xem id_user đã có trong giỏ hàng chưa
+    const queryCheckCart =
+      "SELECT id, quantity FROM Cart WHERE id_user = @idUser AND idProductSku = @idProductSku";
+
+    const checkCartResult = await database
+      .request()
+      .input("idUser", userResult.recordset[0].id)
+      .input("idProductSku", idProductSku)
+      .query(queryCheckCart);
+
+    if (checkCartResult.recordset.length !== 0) {
+      // Nếu đã có, cập nhật số lượng
+      const updatedQuantity = checkCartResult.recordset[0].quantity + quantity;
       const queryUpdateCart =
         "UPDATE Cart SET quantity = @updatedQuantity WHERE id = @idCart";
+
       await database
         .request()
         .input("updatedQuantity", updatedQuantity)
-        .input("idCart", exitCartResult.recordset[0].id)
+        .input("idCart", checkCartResult.recordset[0].id)
         .query(queryUpdateCart);
     } else {
-      // Nếu sản phẩm chưa có trong giỏ hàng, thêm mới
+      // Nếu chưa có, thêm mới
       const queryCart =
         "INSERT INTO Cart(id_user, quantity, idProductSku) VALUES (@idUser, @quantity, @idProductSku)";
+
       await database
         .request()
         .input("idUser", userResult.recordset[0].id)
@@ -51,7 +56,7 @@ router.post("/add-cart", checkAuth, checkRole, async (request, response) => {
         .query(queryCart);
     }
 
-    response.status(201).json({
+    response.status(200).json({
       status: 200,
       message: "Add Cart successful",
     });
@@ -59,7 +64,7 @@ router.post("/add-cart", checkAuth, checkRole, async (request, response) => {
     // Xử lý lỗi cụ thể
     if (error.code === "EREQUEST") {
       return response.status(500).json({
-        error: "Database error",
+        error: "Not Exist Product",
       });
     }
 
@@ -76,37 +81,73 @@ router.post(
   async (request, response) => {
     try {
       const idCart = request.body.cartID;
-      const quantity = request.body.quantity;
+      const quantity = Number(request.body.quantity);
 
       // Kiểm tra dữ liệu đầu vào
-      if (!idCart || !quantity || quantity <= 0) {
+      if (!idCart || quantity === undefined || quantity === null) {
         return response.status(400).json({
           error: "Invalid input data",
         });
       }
 
-      // Cập nhật quantity bằng cách cộng thêm vào giá trị hiện tại
-      const queryCart =
-        "UPDATE Cart SET quantity = quantity + @quantity WHERE id = @idCart";
-      const cartResult = await database
+      // Kiểm tra xem id_user của người dùng có khớp với id_user trong Cart hay không
+      const queryUser = "SELECT id FROM [User] WHERE id_account = @idAccount";
+      const userResult = await database
         .request()
-        .input("quantity", quantity)
+        .input("idAccount", request.userData.uuid)
+        .query(queryUser);
+
+      const queryCart = "SELECT id_user FROM Cart WHERE id = @idCart";
+      const cartUserResult = await database
+        .request()
         .input("idCart", idCart)
         .query(queryCart);
 
-      response.status(200).json({
-        status: 200,
-        message: "Update quantity cart success",
-        cart: {
-          cartID: idCart,
-          quantity: quantity, // Giá trị mới của quantity
-        },
-      });
+      if (
+        cartUserResult.recordset.length === 0 ||
+        cartUserResult.recordset[0].id_user !== userResult.recordset[0].id
+      ) {
+        // Nếu không khớp id_user, trả về lỗi
+        return response.status(403).json({
+          error: "Permission denied",
+        });
+      }
+
+      // Tiếp tục xử lý nếu kiểm tra thành công
+
+      if (quantity <= 0) {
+        // Nếu quantity <= 0 thì xóa sản phẩm khỏi giỏ hàng
+        const deleteQuery = "DELETE FROM Cart WHERE id = @idCart";
+        await database.request().input("idCart", idCart).query(deleteQuery);
+
+        response.status(200).json({
+          status: 200,
+          message: "Delete success",
+        });
+      } else {
+        // Cập nhật quantity bằng cách cộng thêm vào giá trị hiện tại
+        const updateQuery =
+          "UPDATE Cart SET quantity = @quantity WHERE id = @idCart";
+        const cartResult = await database
+          .request()
+          .input("quantity", quantity)
+          .input("idCart", idCart)
+          .query(updateQuery);
+
+        response.status(200).json({
+          status: 200,
+          message: "Update quantity cart success",
+          cart: {
+            cartID: idCart,
+            quantity: quantity,
+          },
+        });
+      }
     } catch (error) {
       // Xử lý lỗi cụ thể
       if (error.code === "EREQUEST") {
         return response.status(500).json({
-          error: "Database error",
+          error: "Not Exist Product",
         });
       }
 
@@ -136,11 +177,9 @@ router.get(
         .query(queryCart);
 
       var carts = [];
-
       for (var i = 0; i < cartResult.recordset.length; i++) {
         var attributes = [];
         var media = [];
-
         const queryProductSku =
           "SELECT * FROM ProductSku WHERE id = @idProductSku";
         var productSkuResult = await database
@@ -153,14 +192,22 @@ router.get(
           .request()
           .input("idProduct", productSkuResult.recordset[0].idProduct)
           .query(queryProduct);
-
         if (productSkuResult.recordset[0].idAttributeValue1 === null) {
-          const queryMedia = "SELECT linkString FROM Media WHERE id = @id";
+          const queryMedia = "SELECT * FROM Media WHERE id_product = @id";
           var mediaResult = await database
             .request()
             .input("id", productResult.recordset[0].id)
             .query(queryMedia);
-          media.push(mediaResult.recordset[0].linkString);
+          var image = {
+            mediaID: "13",
+            linkString: mediaResult.recordset[0].linkString,
+            title: "test",
+            description: "test",
+            objectRefType: 0,
+            mediaType: 0,
+            objectRefID: "1",
+          };
+          media.push(image);
         } else {
           if (productSkuResult.recordset[0].idAttributeValue1 !== null) {
             const queryAttributeValue =
@@ -172,8 +219,16 @@ router.get(
                 productSkuResult.recordset[0].idAttributeValue1
               )
               .query(queryAttributeValue);
-
-            media.push(attributeValueResult.recordset[0].linkString);
+            var image = {
+              mediaID: "13",
+              linkString: attributeValueResult.recordset[0].linkString,
+              title: "test",
+              description: "test",
+              objectRefType: 0,
+              mediaType: 0,
+              objectRefID: "1",
+            };
+            media.push(image);
 
             const queryAttribute =
               "SELECT * FROM ProductAttribute WHERE id = @id";
@@ -225,18 +280,24 @@ router.get(
         var cart = {
           cartID: cartResult.recordset[i].id,
           productID: productSkuResult.recordset[0].idProduct,
+          productName: productResult.recordset[0].name,
           productDescription: productResult.recordset[0].decription,
           productSKUID: cartResult.recordset[i].idProductSku,
           medias: media,
           quantity: cartResult.recordset[i].quantity,
-          price: productSkuResult.recordset[0].price,
+          price: productSkuResult.recordset[0].price + "",
+          priceBefore: 0,
           attribute: attributes,
         };
         carts.push(cart);
       }
       response.status(200).json(carts);
     } catch (error) {
-      console.log(error);
+      if (error.code === "EREQUEST") {
+        return response.status(500).json({
+          error: "Not Exist Product",
+        });
+      }
       response.status(500).json({
         error: "Internal Server Error",
       });
